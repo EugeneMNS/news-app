d73393dbd896d386812b7a06eb3e48ab512a112d
EugeneMNS <evgen.str4@gmail.com>
2022-09-12 13:19:00 +0300

    finish

--- App.js
+++ App.js
@@ -1,35 +1,7 @@
-import {StatusBar} from 'expo-status-bar';
-import {Text, View, Alert} from 'react-native';
-import {Post} from "./components/Post";
-import React from "react";
-import axios from "axios";
-
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { Navigation } from './screens/Navigation';
 
 export default function App() {
-    const [items, setItems] = React.useState();
-
-    React.useEffect(()=>{
-        axios.get('https://5c3755177820ff0014d92711.mockapi.io/articles/')
-            .then(({ data }) =>{
-                setItems(data);
-            })
-            .catch(err => {
-                console.log(err)
-                Alert.alert('Error', 'Something went wrong')
-                //alert('Error to fetch articles')
-        })
-    }, [])
-
-    return (
-        <View>
-            {items.map((obj) => (
-                <Post
-                title={obj.title}
-                createdAt={obj.createdAt}
-                imageUrl={obj.imageUrl}
-                />
-            ))}
-            <StatusBar style="auto" />
-        </View>
-    );
+  return <Navigation />;
 }
--- components/Post.jsx
+++ components/Post.jsx
@@ -1,7 +1,6 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 const PostView = styled.View`
-  margin-top: 20px;
   flex-direction: row;
   padding: 15px;
   border-bottom-width: 1px;
@@ -21,25 +20,35 @@ const PostTitle = styled.Text`
   font-weight: 700;
 `;
 
+const PostDetails = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
 const PostDate = styled.Text`
   font-size: 12px;
   color: rgba(0, 0, 0, 0.4);
   margin-top: 2px;
 `;
 
-const PostDetails = styled.View`
-  justify-content: center;
-`;
+const truncateTitle = (str) => {
+  if (str.length >= 50) {
+    return str.substring(0, 50) + '...';
+  }
 
-export const Post = ({title, imageUrl, createdAt}) =>{
-    return(
-        <PostView>
-            <PostImage
-                source={{uri: imageUrl}}
-            />
-            <PostDetails>
-                <PostTitle>{title}</PostTitle>
-                <PostDate>{createdAt}</PostDate>
-            </PostDetails>
-        </PostView>
-)}
+  return str;
+};
+
+// date-fns => format
+
+export const Post = ({ title, imageUrl, createdAt }) => {
+  return (
+    <PostView>
+      <PostImage source={{ uri: imageUrl }} />
+      <PostDetails>
+        <PostTitle>{truncateTitle(title)}</PostTitle>
+        <PostDate>{new Date(createdAt).toLocaleDateString()}</PostDate>
+      </PostDetails>
+    </PostView>
+  );
+};
