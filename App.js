59a233bffee3d1fc0221f419975308b353c3ed4f
EugeneMNS <evgen.str4@gmail.com>
2022-09-12 13:14:48 +0300

    init posts

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Text, View, Alert} from 'react-native';
+import {Post} from "./components/Post";
+import React from "react";
+import axios from "axios";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [items, setItems] = React.useState();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    React.useEffect(()=>{
+        axios.get('https://5c3755177820ff0014d92711.mockapi.io/articles/')
+            .then(({ data }) =>{
+                setItems(data);
+            })
+            .catch(err => {
+                console.log(err)
+                Alert.alert('Error', 'Something went wrong')
+                //alert('Error to fetch articles')
+        })
+    }, [])
+
+    return (
+        <View>
+            {items.map((obj) => (
+                <Post
+                title={obj.title}
+                createdAt={obj.createdAt}
+                imageUrl={obj.imageUrl}
+                />
+            ))}
+            <StatusBar style="auto" />
+        </View>
+    );
+}
